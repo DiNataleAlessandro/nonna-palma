fee9a3794e5f6f246ce8ab924f9d9795c0530433
DiNataleAlessandro <alessdn03@gmail.com>
2026-03-21 22:35:53 +0100

    fix: isolate transparent SVG as unique favicon source to avoid square background in browser

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#A05222',
     icons: [
       {
-        src: '/icon.png',
+        src: '/pwa-icon.png',
         sizes: '512x512',
         type: 'image/png',
       },
       {
-        src: '/apple-icon.png',
+        src: '/apple-touch-icon.png',
         sizes: '180x180',
         type: 'image/png',
       }
